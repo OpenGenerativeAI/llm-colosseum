0e3b8b6ba4118990bce6eee27fcf1a0ebf90ca9d
Pierre-LouisBJT <plbiojout@gmail.com>
2024-03-23 12:30:38 -0700

    Added LLM API calls

--- language_models.py
+++ language_models.py
@@ -1,38 +1,16 @@
 import os
 from typing import Tuple
 
-import phospho.config as config
-
 try:
     from openai import AsyncOpenAI, OpenAI
 except ImportError:
     pass
 
 
-def get_provider_and_model(model: str) -> Tuple[str, str]:
-    """
-    Get the provider and model from a string in the format "provider:model"
-    If no provider is specified, it defaults to "openai"
-
-    Args:
-        model (str): The model string in the format "provider:model"
-
-    Returns:
-        tuple: A tuple with the provider and model
-    """
-    # If the OVERRIDE_WITH_OLLAMA_MODEL environment variable is set, use the Ollama model
-    # in all cases. This is used in the preview mode or for tests.
-    if config.OVERRIDE_WITH_OLLAMA_MODEL is not None:
-        ollama_model = config.OVERRIDE_WITH_OLLAMA_MODEL
-        return "ollama", ollama_model
-
-    split_result = model.split(":")
-    if len(split_result) == 1:
-        return "openai", split_result[0]
-    return split_result[0], split_result[1]
-
-
 def get_async_client(provider: str) -> AsyncOpenAI:
+    """
+    Provider can be "openai", "mistral" or "ollama".
+    """
     if provider == "openai":
         return AsyncOpenAI()
     if provider == "mistral":
